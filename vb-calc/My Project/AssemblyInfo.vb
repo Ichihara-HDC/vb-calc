6cb3ec08dd768bd375fd4efecb9cebbaaf3bd425
Ichihara <hdc242302@gmail.com>
2025-05-15 13:43:28 +0900

    新規プロジェクトの作成

---
+++ vb-calc/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下を通して制御されます
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します
+
+<Assembly: AssemblyTitle("vb-calc")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Dynabook")>
+<Assembly: AssemblyProduct("vb-calc")>
+<Assembly: AssemblyCopyright("Copyright © Dynabook 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+<Assembly: Guid("8feafe18-a6d5-4b57-b94f-6b8bf319de87")>
+
+' アセンブリのバージョン情報は次の 4 つの値で構成されています:
+'
+'      メジャー バージョン
+'      マイナー バージョン
+'      ビルド番号
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
