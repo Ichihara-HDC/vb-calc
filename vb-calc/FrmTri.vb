f7a80b51a31b3f21d5fb2446bf678e2726171492
Ichihara <hdc242302@gmail.com>
2025-05-15 14:11:50 +0900

    Form2コーディング完成

--- vb-calc/FrmTri.vb
+++ vb-calc/FrmTri.vb
@@ -1,3 +1,11 @@
 ﻿Public Class FrmTri
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Dim teihen As Integer = txtTeihen.Text
+        Dim takasa As Integer = txtTakasa.Text
+        Dim menseki As Integer
 
+        menseki = teihen * takasa
+        lblMenseki.Text = menseki
+
+    End Sub
 End Class
